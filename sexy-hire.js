17d63965854455e02cef897e516988caf125efc0
Marco Gallen <mgallen@OCCQRO.local>
2016-09-07 16:02:51 -0500

    Corregido error del puerto para loguearse con FB

--- sexy-hire.js
+++ sexy-hire.js
@@ -35,9 +35,9 @@ app.get('/confirma-datos',function(req,res){
 	res.render('confirma_datos');
 });
 
-var server = app.listen(8081,function(){
+var server = app.listen(80, function(){
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log("Example app listening at http://%s:%s", host, port);
+	console.log("Example app listening at http:// %s:%s", host, port);
 })
