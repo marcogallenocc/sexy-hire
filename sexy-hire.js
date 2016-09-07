37c32b31fe88c56813859b7b5e61460a26e8efcc
Marco Gallen <mgallen@OCCQRO.local>
2016-09-07 16:48:09 -0500

    Se elimino el user y psw del master, se cambio por un Salir con liga /logout

--- sexy-hire.js
+++ sexy-hire.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-
-
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
