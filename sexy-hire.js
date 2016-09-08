0e91445cd1858314bb0750f67aa27601ab8180c4
Marco Gallen <mgallen@OCCQRO.local>
2016-09-07 21:04:51 -0500

    Pagina de login, pagina de instagram

--- sexy-hire.js
+++ sexy-hire.js
@@ -1,20 +1,59 @@
-var express = require('express');
-var app = express();
+//Dependencias
 
+var express = require('express');
+var request = require('request');
+var ig = require('instagram-node').instagram();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
+var app = express();
+
+
+//Sistema de templates
 app.engine('handlebars', handlebars.engine);
 
 app.set('view engine','handlebars');
 
+
+/** INSTAGRAM ***/
+ig.use({access_token:"TBD"});
+ig.use({client_id: '19dbcc28737143d18193f0c2a312ebc1', client_secret:'302665a7671f4c67ac873bf85e1831d7'});
+
+var redirect_uri = 'http://localhost/handleauth';
+
+exports.authorize_user = function(req, res){
+	res.redirect(ig.get_authorization_url(redirect_uri));
+}
+
+exports.handleauth = function(req,res){
+	ig.authorize_user(req.query.code,redirect_uri, function(err, result){
+		if(err){
+			console.log(err.body);
+			res.send("Fuckity fuckity foo");
+		} else {
+			console.log(result.access_token);
+			var url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + result.access_token;
+
+		    request(url, function(err, response, body){
+		        var imgs = JSON.parse(body);
+		        console.log(imgs.data[0].images.thumbnail.url);
+		        var data = imgs.data;
+		        var thumbnail = imgs.data[0].images.thumbnail.url;
+		        res.render('insta_conectado', {datum: data});
+		    });
+		}
+	});
+};
+
+/*** ROUTING ***/
+//Ruta de archivos estaticos
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
 	res.render('index');
 });
 
-app.get('/',function(req,res){
-	res.render('index');
+app.get('/login',function(req,res){
+	res.render('login');
 });
 
 app.get('/selector',function(req,res){
@@ -32,6 +71,10 @@ app.get('/acerca-de-ti',function(req,res){
 app.get('/confirma-datos',function(req,res){
 	res.render('confirma_datos');
 });
+
+//INSTAGRAM AUTH USER & GET TOKEN 
+app.get('/authorize_user', exports.authorize_user);
+app.get('/handleauth', exports.handleauth);
 
 var server = app.listen(80, function(){
 	var host = server.address().address;
